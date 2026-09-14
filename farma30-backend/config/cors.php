e6d4cd27b63104ad3f74602b33520ae8f9df220a
shadowsssss6-coder <shadowsssss6@gmail.com>
2026-09-13 19:18:10 -0500

    arreglo en cors

--- farma30-backend/config/cors.php
+++ farma30-backend/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => 'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
